4e25ea249b990de608f0fe77270d1858659c2355
Adithya K <a.dty.3403@gmail.com>
2022-08-25 20:39:32 +0530

    Update MirrorNumber

--- RECURSION/sp05_MirrorNumber.java
+++ RECURSION/sp05_MirrorNumber.java
@@ -4,7 +4,7 @@
 
 import java.util.*;
 
-public class mirrorNumber {
+public class sp05_MirrorNumber {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
