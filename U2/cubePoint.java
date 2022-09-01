82e996a5b1b84c7f0f1c3f23e38f25c0c77385c5
adithya <a.dty.3403@gmail.com>
2022-09-01 20:04:35 +0530

    added cubePoint.java

--- U2/cubePoint.java
+++ U2/cubePoint.java
@@ -33,7 +33,7 @@
 
 import java.util.*;
 
-public class Solution {
+public class cubePoint {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
