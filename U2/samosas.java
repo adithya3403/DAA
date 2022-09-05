cca0e1e273c97b0b08743d623e216dd2765d1734
adithya3403 <a.dty.3403@gmail.com>
2022-09-05 16:40:43 +0530

    updated samosas.java

--- U2/samosas.java
+++ U2/samosas.java
@@ -6,37 +6,42 @@ public class samosas {
         String[] str = sc.nextLine().split(" ");
         int n = str.length;
         int[] box = new int[n];
-        for (int i = 0; i < n; i++)
+        for (int i = 0; i < n; i++) {
             box[i] = Integer.parseInt(str[i]);
+        }
         int t = sc.nextInt();
         System.out.println(minSpeed(box, t));
         sc.close();
     }
 
+    static int maximum(int[] arr) {
+        int max = arr[0];
+        for (int i : arr) {
+            if (i > max) {
+                max = i;
+            }
+        }
+        return max;
+    }
+
     static int minSpeed(int[] box, int t) {
-        int l = 0, r = maximum(box);
+        int l = 1, r = maximum(box);
         while (l < r) {
             int mid = (l + r) / 2;
-            if (isPossible(box, t, mid))
+            if (isMin(box, t, mid)) {
                 r = mid;
-            else
+            } else {
                 l = mid + 1;
+            }
         }
         return l;
     }
 
-    static boolean isPossible(int[] box, int t, int speed) {
-        int time = 0;
-        for (int i = 0; i < box.length; i++)
-            time += ((box[i] - 1) / speed) + 1;
-        return time <= t;
-    }
-
-    static int maximum(int[] box) {
-        int max = box[0];
-        for (int i = 1; i < box.length; i++)
-            if (box[i] > max)
-                max = box[i];
-        return max;
+    static boolean isMin(int[] box, int t, int mid) {
+        int h = 0;
+        for (int i = 0; i < box.length; i++) {
+            h += (box[i] + mid - 1) / mid;
+        }
+        return h <= t;
     }
 }
