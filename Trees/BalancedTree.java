1f8422924e053995660671d3ee9cb2907f7b0c4d
adithya3403 <a.dty.3403@gmail.com>
2022-09-21 13:45:51 +0530

    Update BalancedTree.java

--- Trees/BalancedTree.java
+++ Trees/BalancedTree.java
@@ -45,8 +45,6 @@ false
 
 */
 
-import java.util.*;
-
 class BinaryTreeNode {
     public int data;
     public BinaryTreeNode left, right;
@@ -77,47 +75,5 @@ class Solution {
         if (root == null || root.data == -1)
             return 0;
         return 1 + Math.max(height(root.left), height(root.right));
-    }
-}
-
-public class BalancedTree {
-    static BinaryTreeNode root;
-    static BinaryTreeNode temp = root;
-
-    void insert(BinaryTreeNode temp, int key) {
-        if (temp == null) {
-            root = new BinaryTreeNode(key);
-            return;
-        }
-        Queue<BinaryTreeNode> q = new LinkedList<BinaryTreeNode>();
-        q.add(temp);
-        // Do level order traversal until we find
-        // an empty place.
-        while (!q.isEmpty()) {
-            temp = q.peek();
-            q.remove();
-            if (temp.left == null) {
-                temp.left = new BinaryTreeNode(key);
-                break;
-            } else
-                q.add(temp.left);
-            if (temp.right == null) {
-                temp.right = new BinaryTreeNode(key);
-                break;
-            } else
-                q.add(temp.right);
-        }
-    }
-
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        String str[] = sc.nextLine().split(" ");
-        BalancedTree st = new BalancedTree();
-        root = new BinaryTreeNode(Integer.parseInt(str[0]));
-        for (int i = 1; i < str.length; i++)
-            st.insert(root, Integer.parseInt(str[i]));
-        Solution sol = new Solution();
-        System.out.println(sol.isBalanced(root));
-        sc.close();
     }
 }
