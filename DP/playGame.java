4ff7720782dc0848d446978716514aae94f1e002
adithya3403 <a.dty.3403@gmail.com>
2022-10-17 19:12:35 +0530

    Update playGame.java

--- DP/playGame.java
+++ DP/playGame.java
@@ -54,11 +54,12 @@ Explanation:
 class playGame {
     public int completeLevels(int n) {
         // implement your code here.
-        int[] dp = new int[n + 2];
+        int[] dp = new int[n + 1];
         dp[0] = 1;
-        dp[1] = 2;
-        for (int i = 2; i <= n + 1; i++)
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
             dp[i] = dp[i - 1] + dp[i - 2];
-        return dp[n - 1];
+        }
+        return dp[n];
     }
 }
