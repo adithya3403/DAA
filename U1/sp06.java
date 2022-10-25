5da01606c208f05145514bde6d3b73298ddb94b8
adithya3403 <a.dty.3403@gmail.com>
2022-10-25 16:00:22 +0530

    Strobogrammatic number II

--- U1/sp06.java
+++ U1/sp06.java
@@ -3,33 +3,31 @@
 // input 2 output [11, 69, 88, 96]
 
 import java.util.*;
+
 public class sp06 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        System.out.println(uTurnNums(n));
+        System.out.println(findStrobogrammatic(n, n));
         sc.close();
     }
-    public static Set<String> uTurnNums(int n) {
-        Set<String> res = new TreeSet<>();
-        if (n == 0) {
-            res.add("");
-            return res;
+
+    static char[][] digitPair = { { '1', '1' }, { '8', '8' }, { '6', '9' }, { '9', '6' } };
+
+    static List<String> findStrobogrammatic(int k, int n) {
+        if (k <= 0)
+            return new ArrayList<String>(Arrays.asList(""));
+        if (k == 1)
+            return new ArrayList<>(Arrays.asList("0", "1", "8"));
+        List<String> subList = findStrobogrammatic(k - 2, n);
+        List<String> result = new ArrayList<>();
+        for (String str : subList) {
+            if (k != n)
+                result.add("0" + str + "0");
+            for (char[] aDigitPair : digitPair)
+                result.add(aDigitPair[0] + str + aDigitPair[1]);
         }
-        if (n == 1) {
-            res.add("0");
-            res.add("1");
-            res.add("8");
-            return res;
-        }
-        Set<String> rem = uTurnNums(n - 2);
-        for (String s : rem) {
-            res.add('0' + s + '0');
-            res.add('1' + s + '1');
-            res.add('6' + s + '9');
-            res.add('8' + s + '8');
-            res.add('9' + s + '6');
-        }
-        return res;
+        Collections.sort(result);
+        return result;
     }
 }
