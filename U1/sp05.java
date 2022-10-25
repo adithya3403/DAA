905d3cea9c99d001e703459904fb721d077cb0fd
adithya3403 <a.dty.3403@gmail.com>
2022-10-25 17:00:58 +0530

    GCD

--- U1/sp05.java
+++ U1/sp05.java
@@ -1,7 +1,52 @@
-// GCD of min and max
-// Input: 16 8 12 24 36 Output: 4
-// minH is 8 and maxH is 36, and the HCF(8, 36) is 4
-// Input: 19 23 2 5 7 9 11 13 Output: 1
+// sp05 GCD
+
+/*
+
+Mr.Aravind is provided a list of various building heights in a city, heights[], 
+which consists of all positive integers.
+
+Mr.Aravind has two assignments. They are as follows: 
+- Calculate the minimum height-minH and maximum height-maxH from heights[]. 
+- Determine the highest common factor between minH and MaxH.
+
+Your task is to help Mr.Aravind to complete his assignments and 
+to return the HCF of minH and maxH.
+
+Input Format:
+-------------
+Space separated integers, heights of the buildings, heights[].
+
+Output Format:
+--------------
+Print an integer result, HCF of minH and maxH.
+
+
+Sample Input-1:
+---------------
+16 8 12 24 36
+
+Sample Output-1:
+----------------
+4
+
+Explanation:
+------------
+minH is 8 and maxH is 36, and the HCF(8, 36) is 4
+
+
+Sample Input-2:
+---------------
+19 23 2 5 7 9 11 13
+
+Sample Output-2:
+----------------
+1
+
+Explanation:
+------------
+minH is 2 and maxH is 23,and HCF(2,23)is 1
+
+*/
 
 import java.util.*;
 
