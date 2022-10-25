2927becefc8d4cb9a9305df8067dbf62b74fe32f
adithya3403 <a.dty.3403@gmail.com>
2022-10-25 17:00:45 +0530

    String

--- U1/sp03.java
+++ U1/sp03.java
@@ -1,6 +1,55 @@
-// Reverse a string
-// input divide output edivid
-// input racing output gnicar
+// sp03 String
+
+/*
+
+Ms Sonali Sharma is given a task. She has an habit of reading the words 
+from the end to the start (i.e., if the original word is Hello,  
+she reads it as "olleH").
+
+Your task is to help her to read the word properly, 
+by following the rules:
+    - You are given a word W,
+    - Rearrange the characters from end to start in the word.
+    - And print the word.
+
+NOTE: 
+ - W contains only lowercase letters.
+ 
+Input Format:
+-------------
+A string W, word.
+
+Output Format:
+--------------
+Print a string result.
+
+
+Sample Input-1:
+---------------
+divide
+
+Sample Output-1:
+----------------
+edivid
+
+Explanation:
+------------
+After rearranging the characters from end to start, the word is : edivid 
+
+
+Sample Input-2:
+---------------
+racing
+
+Sample Output-2:
+----------------
+gnicar
+
+Explanation:
+------------
+After rearranging the characters from end to start, the word is : gnicar 
+
+*/
 
 import java.util.*;
 
