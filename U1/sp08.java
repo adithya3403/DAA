3d957f9b398c027e27e005c99d1ccd0f98158790
adithya3403 <a.dty.3403@gmail.com>
2022-10-25 17:01:30 +0530

    P_power_N

--- U1/sp08.java
+++ U1/sp08.java
@@ -1,26 +1,49 @@
-// A school kid named X, wants to find the value of N^P
-// Pth power of a number N.
-// You will be given two integers N and P.
-// Your task is to help X to find the value of N^P.
-// NOTE:
-// Value of N and P can be both positive and negative
+// sp08 P_power_N
 
-// Input Format:
-// Two integers N and P, where N is the number and P is the power
-// Output Format:
-// Print a float number, N power P
+/*
 
-// Sample Input-1:
-// 3 2
-// Sample Output-1:
-// 9.000000
+A school kid named Chitti Naidu, wants to find the value of N^P
+Pth power of a number N.
 
-// Sample Input-2:
-// 2 -3
-// Sample Output-2:
-// 0.125000
+You will be given two integers N and P.
+Your task is to help Chitti Naidu to find the value of N^P.
+
+NOTE:
+---------
+Value of N and P can be both positive and negative
+
+
+Input Format:
+-------------
+Two integers N and P, where N is the number and P is the power
+
+Output Format:
+--------------
+Print a float number, N power P
+
+
+Sample Input-1:
+---------------
+3 2
+
+Sample Output-1:
+----------------
+9.000000
+
+
+Sample Input-2:
+---------------
+2 -3
+
+Sample Output-2:
+----------------
+0.125000
+
+
+*/
 
 import java.util.*;
+
 public class sp08 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -29,9 +52,12 @@ public class sp08 {
         System.out.format("%.6f", findPow(x, n));
         sc.close();
     }
+
     public static float findPow(int x, int n) {
-        if (n == 0) return 1;
-        if (n < 0) return 1 / findPow(x, -n);
+        if (n == 0)
+            return 1;
+        if (n < 0)
+            return 1 / findPow(x, -n);
         return x * findPow(x, n - 1);
     }
 }
